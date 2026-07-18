58ac6973d8bb4d3e02bf718e5a9b92b9a22bd362
TebatsoSeshayi <TebatsoSeshayiOut@outlook.com>
2026-07-18 11:01:45 +0200

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,24 @@
 import type { NextConfig } from "next";
+import path from "path";
 
-const isProd = process.env.NODE_ENV === "production";
+const isGitHubPages = process.env.GITHUB_ACTIONS === "true";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
+  ...(isGitHubPages && {
+    output: "export",
+    trailingSlash: true,
+    images: {
+      unoptimized: true,
+    },
+    basePath: "/tebatso-portfolio",
+    assetPrefix: "/tebatso-portfolio/",
+  }),
+
+  serverExternalPackages: ["pdfjs-dist"],
+
+  turbopack: {
+    root: path.join(__dirname),
   },
-  basePath: isProd ? "/tebatso-portfolio" : "",
-  assetPrefix: isProd ? "/tebatso-portfolio/" : "",
 };
 
 export default nextConfig;
